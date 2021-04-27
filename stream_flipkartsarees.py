644975b3da945ff64c8402a63ac7070445174cba
felixpaul96 <77623326+felixpaul96@users.noreply.github.com>
2021-04-27 11:56:43 -0700

    Update stream_flipkartsarees.py

--- stream_flipkartsarees.py
+++ stream_flipkartsarees.py
@@ -3,7 +3,7 @@ from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
 import numpy as np
-model = load_model('flipkart')
+model = load_model('flipkartsarees')
  
  
  
